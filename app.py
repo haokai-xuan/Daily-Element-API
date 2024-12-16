b00213eb1f8ce845e8155bcb2dcfac3c9d6e3f62
Haokai Xuan <haokai.xuan2006@gmail.com>
2024-12-16 00:26:16 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify
+import datetime
+import random
+from pytz import timezone
+
+from elements import elements
+
+
+app = Flask(__name__)
+
+recent_elements = {}  # {date: element}
+
+
+def get_most_recent_date():
+    most_recent_zone = timezone('Pacific/Kiritimati')
+    now = datetime.datetime.now(most_recent_zone)
+    print(now)
+    return now.strftime('%Y%m%d')  # ISO format (YYYYMMDD)
+
+
+def update_recent_elements():
+    current_date = get_most_recent_date()
+
+    if current_date in recent_elements:
+        return
+
+    seed = int(current_date)
+    random.seed(seed)
+
+    mystery_element = random.choice(elements)
+
+    # Ensure no repeats within an 80-day time frame
+    while mystery_element in recent_elements.values():
+        seed -= 100000
+        random.seed(seed)
+        mystery_element = random.choice(elements)
+
+    recent_elements[current_date] = mystery_element
+
+    if len(recent_elements) > 80:
+        oldest_date = min(recent_elements.keys())
+        del recent_elements[oldest_date]
+
+
+@app.route('/')
+def home():
+    update_recent_elements()
+
+    return jsonify(recent_elements)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
